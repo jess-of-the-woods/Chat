cb98bbdd06845046ca3c5293265f127e43eab89f
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-07-05 17:13:34 +1200

    add index.html, install socket.io, res.sendFile

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var http = require('http').Server(app);
 var port = 3000;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello World</h1>')
+  // res.send('<h1>Hello World</h1>')
+  res.sendFile(__dirname + '/index.html');
 })
 
 http.listen(port, function(){
