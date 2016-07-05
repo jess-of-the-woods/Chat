c04e2433baf01362ada30142437bef185da702b1
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-07-05 20:55:22 +1200

    now working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chat
+
+##What I learned..
+
+I had the script tags inside the head of the HTML. Once I moved the scripts down to just below the form, it all worked as expected. 
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-// var $ = require('jquery')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var port = 3000
+var port = 3000;
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello World</h1>')
-  res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html');
+});
+
+// io.on('connection', function(socket){
+//   console.log('a user connected')
+//   socket.on('chat message', function(msg){
+//     console.log('message: ', + msg)
+//   })
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// })
 
 io.on('connection', function(socket){
-  console.log('a user connected')
   socket.on('chat message', function(msg){
-    console.log('message', + msg)
-  })
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+    io.emit('chat message', msg);
   });
-})
+});
 
 http.listen(port, function(){
-  console.log('Lishning on port ' + port + '! Yep!')
+  console.log('Listnin\' on port ' + port + '! Yep!')
 })
