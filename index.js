35da36f17225a1e07fd749fb5a555b0d7d0b3cde
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-07-05 17:09:19 +1200

    add port var

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+var port = 3000;
+
 app.get('/', function(req, res){
   res.send('<h1>Hello World</h1>')
 })
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on port ' + port + '! Yep!');
 });
